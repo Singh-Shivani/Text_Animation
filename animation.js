132b7b67706b771056099250d021bc521c3c8929
Singh-Shivani <47295558+Singh-Shivani@users.noreply.github.com>
2019-09-01 17:26:12 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,3 +1,5 @@
+// this is to get the total length of each letter
+
 const logo = document.querySelectorAll('#logo path');
 for (let i = 0; i < logo.length; i++) {
     console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
